52b6cf5fe9f4bdd02ebfcb0dfa54bde32c97e86c
Swapnil-Srivastava273 <31892718+Swapnil-Srivastava273@users.noreply.github.com>
2019-05-04 19:55:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 let http=require("http");
 http.createServer((req,res)=>{
    res.writeHead(200,{"content-type":"text/html;charset=utf-8"});
-   setInterval(()=>{res.write("😂")},0);
+   setInterval(()=>{res.write("😂")},100);
 }).listen(process.env.PORT||5000);
