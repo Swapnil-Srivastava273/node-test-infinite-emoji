3945d265ffe9c1b524479c2098c48c5b33f1663d
Swapnil-Srivastava273 <31892718+Swapnil-Srivastava273@users.noreply.github.com>
2019-05-04 20:22:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 "use strict"
 let http=require("http");
-let emoji=["😂",'😀','😅','🤓','😜','😎','🙂','😊','😐','😄','☹','😇','😒','🤔','😝','😊','😑','😶','😲','😰','😓','😱','😖','😌','😫','🙄','😑','😈','😵','🤧','🤑',"🤤"]
+const emoji=["😂",'😀','😅','🤓','😜','😎','🙂','😊','😐','😄','☹','😇','😒','🤔','😝','😊','😑','😶','😲','😰','😓','😱','😖','😌','😫','🙄','😑','😈','😵','🤧','🤑',"🤤"];
 http.createServer((req,res)=>{
-   res.writeHead(200,{"content-type":"text/html;charset=utf-8"});
-   setInterval(()=>{res.write(emoji[Math.floor(Math.random()*emoji.length)])},100);
+    res.writeHead(200,{"content-type":"text/html;charset=utf-8"});
+    let interval=setInterval(()=>{res.write(emoji[Math.floor(Math.random()*emoji.length)]);},100);
+    req.on('close',()=>{
+        clearInterval(interval);
+    });
 }).listen(process.env.PORT||5000);
