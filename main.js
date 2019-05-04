5acc1c9c44a57b8032c7ac5426d21a1c065c9da8
Swapnil-Srivastava273 <31892718+Swapnil-Srivastava273@users.noreply.github.com>
2019-05-04 20:05:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict"
 let http=require("http");
+let emoji=["😂",'😀','😅','🤓','😜','😎','🙂','😊','😐','😄','☹','😇','😒','🤔','😝','😊','😑','😶',😲','😰','😓','😱','😖','😌','😫','🙄','😑','😈','😵','🤧','🤑',"🤤"]
 http.createServer((req,res)=>{
    res.writeHead(200,{"content-type":"text/html;charset=utf-8"});
-   setInterval(()=>{res.write("😂")},100);
+   setInterval(()=>{res.write(emoji[Math.floor(Math.random()*emoji.length)])},100);
 }).listen(process.env.PORT||5000);
