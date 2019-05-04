78336acf654c3187ccd08a234f3a9d17d709baaf
Swapnil-Srivastava273 <31892718+Swapnil-Srivastava273@users.noreply.github.com>
2019-05-04 20:32:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let http=require("http");
 const emoji=["😂",'😀','😅','🤓','😜','😎','🙂','😊','😐','😄','☹','😇','😒','🤔','😝','😊','😑','😶','😲','😰','😓','😱','😖','😌','😫','🙄','😑','😈','😵','🤧','🤑',"🤤"];
 http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/html;charset=utf-8"});
-    res.write(`<!doctype html><head><title>Infinite Emoji Test</title><meta name="viewport",content="width=device-width"/><style>body{background:linear-gradient( 90deg , rgb(191, 64, 64), rgb(204, 166, 51), rgb(128, 191, 64), rgb(64, 191, 128), rgb(64, 191, 191), rgb(64, 64, 191), rgb(191, 64, 149));}</head><body>`
+    res.write(`<!doctype html><head><title>Infinite Emoji Test</title><meta name="viewport",content="width=device-width"/><style>body{background:linear-gradient( 90deg , rgb(191, 64, 64), rgb(204, 166, 51), rgb(128, 191, 64), rgb(64, 191, 128), rgb(64, 191, 191), rgb(64, 64, 191), rgb(191, 64, 149));}</head><body>`);
     let interval=setInterval(()=>{res.write(emoji[Math.floor(Math.random()*emoji.length)]);},100);
     req.on('close',()=>{
         clearInterval(interval);
